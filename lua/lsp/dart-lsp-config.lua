a3d09b216606259184cc5177e42d21cbc18ab8d7
IIIBreakeRIII <ryu990305@icloud.com>
2024-03-03 16:55:14 +0900

    dart lsp

--- lua/lsp/dart-lsp-config.lua
+++ lua/lsp/dart-lsp-config.lua
@@ -1,20 +1,22 @@
-require("lspconfig").dartls.setup({
-	cmd = { "dart", "language-server", "--protocol=lsp" },
-  filetypes = { "dart" },
-  init_options = {
-  	closingLabels = true,
-  	flutterOutline = true,
-    onlyAnalyzeProjectsWithOpenFiles = true,
-    outline = true,
-    suggestFromUnimportedLibraries = true,
-  },
-  -- root_dir = root_pattern("pubspec.yaml"),
-  settings = {
-    dart = {
-      completeFunctionCalls = true,
-      showTodos = true,
-    },
-  },
-  on_attach = function(client, bufnr)
-  end,
-})
+-- require("lspconfig").dartls.setup({
+-- 	cmd = { "dart", "language-server", "--protocol=lsp" },
+--   filetypes = { "dart" },
+--   init_options = {
+--   	closingLabels = true,
+--   	flutterOutline = true,
+--     onlyAnalyzeProjectsWithOpenFiles = true,
+--     outline = true,
+--     suggestFromUnimportedLibraries = true,
+--   },
+--   -- root_dir = root_pattern("pubspec.yaml"),
+--   settings = {
+--     dart = {
+--       completeFunctionCalls = true,
+--       showTodos = true,
+--     },
+-- 		enableSdkFormatter = true,
+-- 		enableSnippets = true,
+--   },
+--   on_attach = function(client, bufnr)
+--   end,
+-- })
