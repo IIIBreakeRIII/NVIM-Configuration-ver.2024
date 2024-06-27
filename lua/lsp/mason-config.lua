8fa2ad8ce1de5344551edcca2afd0f398fb12c71
IIIBreakeRIII <ryu990305@icloud.com>
2024-06-27 21:43:37 +0900

    SYNC

--- lua/lsp/mason-config.lua
+++ lua/lsp/mason-config.lua
@@ -1,39 +1,39 @@
 require("mason").setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "➜",
+      package_uninstalled = "✗"
+      }
+  }
 })
 
 require("mason-lspconfig").setup {
     ensure_installed = {
-			"lua_ls",
-			"rust_analyzer",
-		},
+      "lua_ls",
+      "rust_analyzer",
+    },
 }
 
 require("lspconfig")['lua_ls'].setup({
-	on_attach = on_attach,
-	capabilities = capabilites
+  on_attach = on_attach,
+  capabilities = capabilites
 })
 
 require("lspconfig")['pyright'].setup({
-	on_attach = on_attach,
-	capabilities = capabilites
+  on_attach = on_attach,
+  capabilities = capabilites
 })
 
 require("lspconfig")['clangd'].setup({
-	on_attach = on_attach,
-	capabilities = capabilites
+  on_attach = on_attach,
+  capabilities = capabilites
 })
 
 require("lspconfig")['jdtls'].setup({
-	on_attach = on_attach,
-	capabilities = capabilites,
-	settings = {
+  on_attach = on_attach,
+  capabilities = capabilites,
+  settings = {
     java = {
       configuration = {
         runtimes = {
@@ -49,11 +49,11 @@ require("lspconfig")['jdtls'].setup({
 })
 
 require("lspconfig")['marksman'].setup({
-	on_attach = on_attach,
-	capabilities = capabilites
+  on_attach = on_attach,
+  capabilities = capabilites
 })
 
 -- require("lspconfig")['dartls'].setup({
--- 	on_attach = on_attach,
--- 	capabilities = capabilites
+--  on_attach = on_attach,
+--  capabilities = capabilites
 -- })
